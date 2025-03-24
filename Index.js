d4d1ef39681b3ce42f501f4560f9bd43cf64fbb6
Xanthom <vikneshdeivakumar7@gmail.com>
2025-03-24 22:52:25 +0530

    Create & develope functionality of QR code  generator

--- Index.js
+++ Index.js
@@ -1,2 +1,29 @@
-Heyy there!
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
 
+inquirer
+  .prompt([
+    {
+    message:"Provide your website link:",
+    name:"URL",
+    }
+  ])
+  .then((answers) => {
+
+    const url = answers.URL;
+    var qr_png = qr.image('url');
+    qr_png.pipe(fs.createWriteStream('i_love_qr.png'));
+
+    fs.writeFile('URL.txt', url , (err) => {
+        if (err) throw err;
+          console.log('The file has been saved!');
+      }); 
+})
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
